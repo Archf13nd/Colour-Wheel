7b6f4ad93a250cdab51cb27588a45dd607bb058d
unknown <ideaswrittenonleaves@gmail.com>
2023-07-22 15:07:17 +0100

    Now takes event strings for listeners and state storage

--- src/functions/services/createEvents.js
+++ src/functions/services/createEvents.js
@@ -1,29 +1,36 @@
 const events = {}
 
-const createEvent = (theEvent, func, element) => {
-element.addEventListener(theEvent, func)
-// Assigns the cb without removing any existing methods
-events[theEvent] = {...events[theEvent], eventCB: func}
+const createEvent = (theEvent, cb, element) => {
+  element.addEventListener(theEvent, cb)
+  // Assigns the cb without removing any existing methods
+  events[element.id] = { ...events[element.id] }
+  events[element.id][theEvent] = { ...events[element.id][theEvent], eventCB: cb }
 }
 
-const callFunctions = (e) => {
-for (const key in events[e.type]) {
-    if (key === 'eventCB') {
+// Returns cb function used in event listeners
+// while keeping id in memory with closure
+const createCBfunction = (eventID) => {
+  const id = eventID
+  return (e) => {
+    for (const key in events[id][e.type]) {
+      if (key === 'eventCB') {
         continue
+      }
+      // Gets the events of element with id in function's lexical scope from closure
+      // Gets the type of event registered, e.g. mousemove
+      // Gets and calls each method stored within
+      events[id][e.type][key](e)
     }
-    events[e.type][key](e)
-    }
+  }
 }
 
+const createEvents = (element, ...newEvents) => {
+  newEvents.forEach((e) => {
+    const cb = createCBfunction(element.id)
+    createEvent(e, cb, element)
+  })
 
-
-const createEvents = (element) => {
-
-createEvent('mousemove', callFunctions, element)
-createEvent('mousedown', callFunctions, element)
-createEvent('mouseup', callFunctions, element)
-return events
+  return events
 }
-
 
 export default createEvents
