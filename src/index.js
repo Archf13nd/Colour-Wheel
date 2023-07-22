c5069986c8a9474cf5dc11e5d30a29572d888ae4
unknown <ideaswrittenonleaves@gmail.com>
2023-07-22 15:11:17 +0100

    getElementById for canvas now expected to return canvas not div with nested canvas

--- src/index.js
+++ src/index.js
@@ -8,11 +8,14 @@ import {
 
 import drawTargets from './functions/services/drawTargets.js'
 import mapCoords from './functions/services/mapCoords.js'
+import colourPicker from './functions/services/colourPicker.js'
+import registerElements from './functions/ui/registerElements.js'
 
 const state = {
   canvas: null,
+
   events: {},
-  paths: {},
+  targets: [],
   mouseX: null,
   mouseY: null,
   radius: null,
@@ -21,16 +24,20 @@ const state = {
   harmony: 'triad'
 }
 
-const targetElement = document.getElementById('color-wheel')
-const targetElementHeight = targetElement.offsetHeight
-const targetElementWidth = targetElement.offsetWidth
+registerElements()
 
-state.canvas = document.createElement('canvas')
+state.canvas = document.getElementById('color-wheel')
 state.ctx = state.canvas.getContext('2d')
-const size = Math.min(targetElementHeight, targetElementWidth)
-state.canvas.width = size
-state.canvas.height = size
+const size = Math.max(state.canvas.width, state.canvas.height)
+if (state.canvas.width !== state.canvas.height) {
+  state.canvas.width = size
+  state.canvas.height = size
+}
 state.radius = size / 2
+
+// Set default x and y positions
+state.mouseX = state.radius
+state.mouseY = state.radius - state.canvas.width / 5
 
 // Init conversion map inside mathUtil.js
 createCoordConversionMaps(state.canvas.width, state.canvas.height, size / 2)
@@ -38,42 +45,56 @@ createCoordConversionMaps(state.canvas.width, state.canvas.height, size / 2)
 const wheelImage = generateWheel(state)
 state.ctx.putImageData(wheelImage, 0, 0)
 
-targetElement.insertAdjacentElement('beforeend', state.canvas)
+state.events = createEvents(state.canvas, 'mousedown', 'mouseup', 'mousemove')
 
-state.events = createEvents(state.canvas)
+console.log(state.events)
 
 const handleDraw = () => {
-  if (!state.mousedown) {
-    return
-  }
-  if (!calcCircleBounds(state.mouseX - state.radius, state.mouseY - state.radius, state.radius)) {
-    return
-  }
-  if (state.paths) {
-    state.ctx.putImageData(wheelImage, 0, 0)
-  }
+  state.ctx.putImageData(wheelImage, 0, 0)
+
   const polarCoords = cartesian2Polar(state.mouseX, state.mouseY)
   const r = polarCoords.r
   const theta = polarCoords.theta
-  const mappedCoords = mapCoords(r, theta, state.radius, state.harmony)
-  drawTargets(state.ctx, mappedCoords)
+  // Store paths array in state for colour picker
+  state.targets = mapCoords(r, theta, state.radius, state.harmony)
+
+  drawTargets(state.ctx, state.targets)
+  colourPicker(state.ctx, state.targets)
 }
 
 // Event Actions
-state.events.mousemove.updateMousePos = (e) => {
-  state.mouseX = e.offsetX
-  state.mouseY = e.offsetY
+state.events['color-wheel'].mousemove.updateMousePos = (e) => {
+  if (!state.mousedown) {
+    return
+  }
+  if (calcCircleBounds(e.offsetX - state.radius, e.offsetY - state.radius, state.radius)) {
+    state.mouseX = e.offsetX
+    state.mouseY = e.offsetY
+  }
 }
 
-state.events.mousedown.recordMouseDown = () => {
+state.events['color-wheel'].mousedown.recordMouseDown = () => {
   state.mousedown = true
 
   handleDraw()
 }
-state.events.mouseup.recordMouseUp = () => {
+state.events['color-wheel'].mouseup.recordMouseUp = () => {
   state.mousedown = false
 }
 
-state.events.mousemove.moveTarget = () => {
+state.events['color-wheel'].mousemove.moveTarget = () => {
+  if (state.mousedown) {
+    handleDraw()
+  }
+}
+
+state.events['cw-complementary-btn'].click.changeHarmony = () => {
+  state.harmony = 'complementary'
   handleDraw()
 }
+state.events['cw-triad-btn'].click.changeHarmony = () => {
+  state.harmony = 'triad'
+  handleDraw()
+}
+
+handleDraw()
